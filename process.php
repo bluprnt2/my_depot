320fe0ce78508063043fc55065385404f7dd22ea
Annie0431 <chaturvea7@students.rowan.edu>
2017-03-18 16:40:00 -0400

    Create process.php
    This is just a rough draft of what login page should do. I will keep updating it as I proceed.

---
+++ process.php
@@ -0,0 +1,34 @@
+<?php
+// get values passed from login.php file
+ $username = $_POST['username'];
+ $password = $_POST['password'];
+ 
+ //to prevent mysql injection
+ $username = stripclashes($username);
+ $password = stripclashes($password);
+ $username = mysql_real_escape_string($username);
+ $password = mysql_real_escape_string($password);
+ 
+ //connect to the server and select database(this is subjected to change)
+ mysql_connect("localhost", "root", "");
+ mysql_select_db("login");
+ 
+ // query the database for user
+ $result = mysql_query("select * from users where username = '$username'  and password = '$password'")
+         or die("FAILED TO LOGIN, CLICK ON FORGOT PASSWORD FOR HELP".mysql_error());
+ 
+ $row =  mysql_fetch_array($result);
+ if( $row['username'] ==  $username && $row['password'] == $password) {
+     if($row['admin'] == 1){ 
+         //subjected to changes.Should actually go to admin page
+        echo "Login successful. Welcome Admin!";
+     }
+     else{
+         //subjected to changes.Should actually go to tutor page
+        echo "Login successful. Welcome Tutor!";
+     }
+ }
+ else {
+     echo "Failed to login. Try again or click forgot password.";
+ }
+ ?>
