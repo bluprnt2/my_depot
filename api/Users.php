f5835a25bcd96a11f06e270fb89e8b488c368ccd
Chris Mariani <chris.j.mariani@gmail.com>
2017-03-23 17:47:22 -0400

    Added functions to the Users.php file in the api for future endpoints

--- api/Users.php
+++ api/Users.php
@@ -1,12 +1,4 @@
 <?php
-    function getNotify($userid, $tutorserver) {
-
-    }
-
-    function toggleUserNotify($userid, $tutorserver) {
-
-    }
-
     function addUser($userid, $title, $content, $deptID, $tutorserver) {
         $query = "INSERT INTO Users (userName, firstName, lastName, saltHash, admin) VALUES (?, ?, ?, ?)";
 
@@ -30,6 +22,24 @@
             }
             $stmnt->close();
             return $user;
+        }
+    }
+
+    function getUserNotify($userid, $tutorserver) {
+        $user = getUser($userid, $tutorserver);
+
+        return (boolean) $user['notify'];
+    }
+
+    function toggleUserNotify($userid, $tutorserver) {
+        $new_notify = !getUserNotify($userid, $tutorserver);
+        $query = "UPDATE Users SET notify=? WHERE userID=?";
+
+        $user = array();
+        if($stmnt = $tutorserver->prepare($query)) {
+            $stmnt->bind_param('ii', $new_notify, $userid);
+            $stmnt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+            $stmnt->close();
         }
     }
 ?>
