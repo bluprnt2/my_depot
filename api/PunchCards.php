0ac6ae5e5644ac9b2ea12e93fe06f95648e22b6d
Chris Mariani <marianic0@students.rowan.edu>
2017-04-01 20:25:15 -0400

    Began working on PunchCards endpoints

---
+++ api/PunchCards.php
@@ -0,0 +1,15 @@
+<?php
+    function getPunchCards($user_id, $checkedIn, $startTime, $endTime, $tutorserver){
+
+    }
+
+    function getCheckedIn($user_id, $tutorserver) {
+
+    }
+
+    function toggleCheckedIn($user_id, $tutorserver) {
+
+    }
+
+
+?>
