648b082ad8b25d2a730fcf5379546e2ecf0e5f78
Chris Mariani <marianic0@students.rowan.edu>
2017-04-04 20:48:29 -0400

    Began working on CourseTutors endpoint, still a work-in-progress

---
+++ api/CourseTutors.php
@@ -0,0 +1,40 @@
+<?php
+    //Not Tested
+    function getCourseTutors($courseID, $tutorID) {
+        $query = "SELECT * FROM CourseTutors WHERE courseID=COALESCE(?, courseID) AND userID=COALESCE(?, userID)";
+
+        $tutorids = array();
+        if($stmnt = $tutorserver->prepare($query)) {
+            $stmnt->bind_param('ii', $courseID, $tutorID);
+            $stmnt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+            $result = $stmnt->get_result();
+            while($a = $result->fetch_assoc()) {
+                $tutorids[] = $a;
+            }
+            $stmnt->close();
+            return $tutorids;
+        }
+    }
+
+    //Not Tested
+    function addCourseTutors($courseID, $tutorID) {
+        $query = "INSERT INTO CourseTutors (courseID, userID) VALUES (?, ?)";
+
+        if($stmnt = $tutorserver->prepare($query)) {
+            $stmnt->bind_param('ii', $courseID, $tutorID);
+            $stmnt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+            $stmnt->close();
+        }
+    }
+
+    //Not Tested
+    function delCourseTutors($courseID, $tutorID) {
+        $query = "DELETE FROM CourseTutors WHERE courseID=? AND tutorID=?";
+
+        if($stmnt = $tutorserver->prepare($query)) {
+            $stmnt->bind_param('ii', $courseID, $tutorID);
+            $stmnt->execute() or trigger_error($stmt->error, E_USER_ERROR);
+            $stmnt->close();
+        }
+    }
+?>
