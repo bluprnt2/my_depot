446bc52a8209d080cbbd81aec92d84ba528ce6f5
Chris Mariani <chris.j.mariani@gmail.com>
2017-04-13 12:01:59 -0400

    Modified the settings page to finish adding features, they just need to be implemented

--- public/settings.php
+++ public/settings.php
@@ -15,13 +15,6 @@
     $courses=APIClient::getCourses(null, null, null);
     $users=APIClient::getUser(null);
 ?>
-<style type="text/css">
-    .settings-dropdown {
-        float:none;
-        width: auto;
-        margin-right:0;
-    }
-</style>
 
 <div class="w3-bar w3-border w3-light-grey w3-cell">
     <?php include("navbar.php"); ?>
@@ -29,6 +22,9 @@
 
 <div class="w3-contianer">
     <h1>Settings</h1>
+
+    <!-- Form for adding a class to the database -->
+
     <form id="addClassForm" method="POST">
         <h3>Add Class</h3>
         Class: <input type="text" name="class">
@@ -42,8 +38,12 @@
         ?></select>
         <button type="submit" name="addClass">Add Class</button>
     </form>
+
+    <!-- Form for removing a class from the database -->
+
     <form id="delClassForm" method="POST">
         <h3>Remove Class</h3>
+        <!-- Allows for narrowing down classes by department -->
         Department: <select name="Department" onChange="delClassUpdate()" class="dept-list settings-dropdown">
     		<option value="">Select a Department</option><?php
             foreach($depts as $d) {
@@ -62,11 +62,17 @@
     	</select>
         <button type="submit" name="delClass">Remove Class</button>
     </form>
+
+    <!-- Form for adding a department to the database -->
+
     <form id="addDeptForm" method="POST">
         <h3>Add Department</h3>
         Department: <input type="text" name="deptName">
         <button type="submit" name="delClass">Add Department</button>
     </form>
+
+    <!-- Form for removing a department from the database -->
+
     <form id="delDeptForm" method="POST">
         <h3>Remove Department</h3>
         Department: <select name="Department" class="dept-list settings-dropdown">
@@ -79,6 +85,9 @@
         ?></select>
         <button type="submit" name="delClass">Remove Department</button>
     </form>
+
+    <!-- Form for associating tutors with courses -->
+
     <form id="assocTutorForm" method="POST">
         <h3>Associate Tutor w/ Class</h3>
         Department: <select name="Department" onChange="assocTutorUpdate()" class="dept-list settings-dropdown">
@@ -100,7 +109,6 @@
         Tutor: <select name="Tutor" class="tutor-list settings-dropdown">
     		<option value="">Select a Tutor</option>
     		<?php
-    			//onChange="getFiles(this.value);"
     			foreach($users as $t){
                     if(!$t->getAdmin())
     				    echo "<option value=" . $t->getUserID() . ">". $t->getUsername() ."</option>";
@@ -108,6 +116,45 @@
     		?>
         </select>
         <button type="submit" name="assocTutor">Assign Tutor</button>
+    </form>
+
+    <form action="./register.php">
+        <h3>Add User</h3>
+        <button type="submit">Add new User</button>
+    </form>
+
+    <!-- Form for removing tutors from classes or completely-->
+
+    <form id="delTutorForm" method="POST">
+        <h3>Remove User</h3>
+        Department: <select name="Department" onChange="delTutorUpdate()" class="dept-list settings-dropdown">
+    		<option value="">Select a Department</option><?php
+            foreach($depts as $d) {
+                $name = $d->getName();
+                $id = $d->getID();
+                echo "<option value=" . $id . ">" . $name . "</option>";
+            }
+        ?></select>
+    	Class: <select name="Course" onChange="delTutorUpdate()" class="course-list settings-dropdown">
+    		<option value="">Select a Course</option>
+    		<?php
+    			foreach($courses as $a){
+    				echo "<option value=\"{ 'deptID':" . $a->getDeptID() . ", 'ID':" . $a->getID() . "}\">". $a->getName() ."</option>";
+    			}
+    		?>
+    	</select>
+        Tutor: <select name="Tutor" class="tutor-list settings-dropdown">
+    		<option value="">Select a Tutor</option>
+    		<?php
+    			foreach($users as $t){
+                    if(!$t->getAdmin())
+    				    echo "<option value=" . $t->getUserID() . ">". $t->getUsername() ."</option>";
+    			}
+    		?>
+        </select>
+        <br /><br />
+        <button type="submit" name="delTutorClass">Remove from Course</button>
+        <button type="submit" name="delTutor">Delete Account</button>
     </form>
 </div>
 <script>
@@ -125,11 +172,21 @@
         course.options[0].selected=true;
         hide(course, "'deptID':".concat(dept.value));
     }
+
+    function changeCourse(id) {
+        //Need to implement hide based on coursetutors endpoint
+    }
+
     function delClassUpdate() {
         changedDepartment("delClassForm");
     }
     function assocTutorUpdate(){
         changedDepartment("assocTutorForm");
+    }
+
+    function delTutorUpdate() {
+        changedDepartment("delTutorForm");
+        changeCourse("delTutorForm");
     }
 </script>
 
