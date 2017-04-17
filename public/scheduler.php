b05334ea3516d9c591ee0f12b7aa9b6a1b2a9cdf
Jeremiah <j.consumer@hotmail.com>
2017-04-16 20:57:42 -0400

    Broken Branch?
    Tryying to fix my branch

--- public/scheduler.php
+++ public/scheduler.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <!-- Just tagging for potential usage in Senior Proj-->
 <!-- GNU GPL License -->
@@ -36,6 +37,7 @@
 		scheduler.config.last_hour = 18;
 		scheduler.config.multi_day = true;
 		scheduler.config.date_step = "5"
+		scheduler.config.drag_move = false;
 		//initializing here
 		scheduler.init('scheduler_here', new Date(),"week");
 		scheduler.setLoadMode("week")
@@ -46,60 +48,116 @@
         require_once("../APIClient.php");
         $events = APIClient::getTimeSlots(null, null, null, null, null, null);
 		//Need to implement names for each course ID
-		 $courses = APIClient::getCourses(null, null );
-        $obj_array = array();
+        $e_array = array();
         foreach($events as $e) {
-            $obj_array[] = array(
+            $e_array[] = array(
                 "start_date" => $e['startTime'],
                 "end_date"   => $e['endTime'],
-				"text"		=>  $e['courseID']	
+				"text"		=>  $e['courseID'],
+				"location"	=>  $e['locID']			
 			);
         };	
-
-        echo json_encode($obj_array);
+        echo json_encode($e_array);
 					?>;
+		var cNames = <?php
+		require_once("../APIClient.php");
+		$courses = APIClient::getCourses(null, null);
+		$c_array = array();	
+        	foreach($courses as $co) {
+            $c_array[] = array(
+				"ID" => $co->getID(),
+                "Name" => $co->getName()	
+			);
+        };	
+		 echo json_encode($c_array);
+		?>	
+		var bNames = <?php
+		require_once("../APIClient.php");
+		$buildings = APIClient::getLocations(null, null, null);
+		$b_array = array();	
+        	foreach($buildings as $bld) {
+            $b_array[] = array(
+				"ID" => $bld->getID(),
+                "Name" => $bld->getBuildingName(),
+				"Room" => $bld->getRoomNumber()
+			);
+        };	
+		 echo json_encode($b_array);
+		?>
+		
 		//Handy for testing			
-		//console.log(slots);
-
+		console.log(slots);
+		console.log(bNames);
+		//console.log(cNames);
+		//////////////////////////
+		//adding to calendar
 		slots.forEach(event); 
+		
+		
 		function event(item) {
+
+				var courseName, buildName = "";
+				var index = 0;
+				var flag = true;
+				//discovering equivalent String for Course
+				while (flag == true) {
+					if(cNames[index].ID == item.text){
+						courseName = "(\n " + cNames[index].Name + " )";
+						flag = false;
+					}
+					index++;
+				};		
+				index = 0;
+				//discovering equivalent String for Building
+				while (flag == false) {
+					if(bNames[index].ID == item.location){
+						buildName = "( " + bNames[index].Name + " )" + bNames[index].Room;
+						flag = true;
+					}
+					index++;
+				};
 				scheduler.addEvent({
 				start_date: item.start_date,//"2017-04-16 09:00"
 				end_date:   item.end_date,//"2017-04-16 12:00"
-				text:   	item.text,
+				text:   	courseName + buildName,
+				
 		});
 		}
 	}
 	</script>
 
+=======
+<?php
+$title = "Scheduler";
+include("header.php");
+?>
+    <div class="w3-container w3-yellow">
+        <p></p>  
+    </div>
+    <div class="w3-container">
+	</div>
+<div class="w3-bar w3-border w3-light-grey">
+    <?php
+    include("navbar.php");
+    ?>
+</div>
+>>>>>>> refs/remotes/origin/master
 
-	<body onload="init();" onresize="modSchedHeight()">
-	
-    <style>
-        a img{
-            border: none;
-        }
-        li{
-            list-style: none;
+<style type="text/css" media="screen">
+           html, body{
+            height:90%;
         }
     </style>
-	<script>
-	function modSchedHeight(){
-		var headHeight = 100;
-		var sch = document.getElementById("scheduler_here");
-		sch.style.height = 
-			(parseInt(document.body.offsetHeight)-headHeight)+"px";
-		var contbox = document.getElementById("contbox");
-		contbox.style.width = (parseInt(document.body.offsetWidth)-300)+"px";
-	}
-	</script>
-	<!-- start of banner/ navbar -->
-	<div class="w3-container w3-yellow">
-       <p></p>  
-	</div>
-	 <div class="w3-container">
-	</div>
+<div style="height:50px;background-color:#92543f">
+    <div id="contbox" style="position: relative; font: bold 17px Arial">
+        <div 
+            style="position: absolute; left: 10px; top: -4px; color:#fafafa">
+            <h3>Schedule Viewer</h3></div>
+    </div>
+</div>
+</div>
 
+<<<<<<< HEAD
 	<!--<br> skip a line. -->
 	<div class="w3-bar w3-border w3-light-grey">
    <?php
@@ -178,4 +236,112 @@
 		<div class="dhx_cal_data"></div>		
 	</div>
 	</body>
+=======
+<div id="scheduler_here" class="dhx_cal_container" 
+     style='width:100%;height:100%;'>
+    <div class="dhx_cal_navline">
+        <div class="dhx_cal_prev_button">&nbsp;</div>
+        <div class="dhx_cal_next_button">&nbsp;</div>
+        <div class="dhx_cal_today_button"></div>
+        <div class="dhx_cal_date"></div>
+        <div class="dhx_cal_tab" name="day_tab" 
+             style="right:332px;"></div>
+        <div class="dhx_cal_tab" name="week_tab" 
+             style="right:268px;"></div>
+        <div class="dhx_cal_tab" name="month_tab" 
+             style="right:204px;"></div>	
+
+    </div>
+    <div class="dhx_cal_header"></div>
+    <div class="dhx_cal_data"></div>		
+</div>
+<div class="w3-container">
+    <?php include("footer.php"); ?>
+</div>
+<script>
+    /**
+	* The following Javascript calls the restful API to 
+	* Create all timeslots and populate them in the scheduler.
+	*/
+    document.addEventListener("DOMContentLoaded", function () {
+        init();
+		var slots = <?php
+			require_once("../APIClient.php");
+			$events = APIClient::getTimeSlots(null, null, null, null, null, null);
+			$e_array = array();
+			foreach($events as $e) {
+				$e_array[] = array(
+					"ID"		 => $e['ID'], 
+					"start_date" => $e['startTime'],
+					"end_date"   => $e['endTime'],
+					"text"		 => $e['courseID'],
+					"location"	 => $e['locID']					
+					);
+				};	
+			echo json_encode($e_array);
+			?>;
+			console.log(slots);
+		var cNames = <?php
+			require_once("../APIClient.php");
+			$courses = APIClient::getCourses(null, null);
+			$c_array = array();	
+        	foreach($courses as $co) {
+				$c_array[] = array(
+					"ID" => $co->getID(),
+					"Name" => $co->getName()	
+					);
+				};	
+			echo json_encode($c_array);
+			?>;
+		var bnames = <?php
+			require_once("../APIClient.php");
+			$buildings = APIClient::getLocations(null, null, null);
+			$b_array = array();	
+        	foreach($buildings as $bld) {
+				$b_array[] = array(
+					"ID" => $bld->getID(),
+					"Name" => $bld->getBuildingName(),
+					"Room" => $bld->getRoomNumber()
+					);
+				};	
+			echo json_encode($b_array);
+			?>;
+		slots.forEach(event); 
+		function event(item) {
+				var coursename = "";
+				var index = 0;
+				var flag = true;
+				//discovering/assigning equivalent String for Course
+				while (flag == true) {
+					if(cNames[index].ID == item.text){
+						coursename = cNames[index].Name + " - ";
+						flag = false;
+					}
+					index++;
+				};		
+				index = 0;
+				//discovering/assigning equivalent String for Building
+				while (flag == false) {
+					if(bnames[index].ID == item.location){
+						coursename = coursename + bnames[index].Name + 
+									" " + bnames[index].Room + " " + " ";
+						flag = true;
+					}
+					index++;
+				};
+				//add the actual events and their info to the calendar
+				scheduler.addEvent({
+				start_date: item.start_date,//"yyyy-mm-dd hh:ii"
+				end_date:   item.end_date,//"yyyy-mm-dd hh:ii"
+				text:   	coursename + " Event ID:" + item.ID,
+				});
+		}
+	});
+
+    window.addEventListener("resize", function () {
+        modSchedHeight();
+    });
+</script>
+</body>
+>>>>>>> refs/remotes/origin/master
 </html>
