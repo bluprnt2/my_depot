cf8917ae661c2509ae431e48271bb0bf48083f2c
bluprnt2 <bluprnt2@gmail.com>
2017-05-08 12:16:23 -0400

    cleaning up the git for final submission

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
-# Feedback Form
-## Need to Do
+# Rowan University Drop-in Tutoring Application
 
 
-### Send data to database
-	get courses
-	get tutors
-	requires api endpoints
-	
-	UPDATE:
-	  get courses + tutors work; needs more testing
-	  need to define POST function for sending completed form to db
-	  
-	
-### Captcha
-	requires open source captcha tool	
-	
-### Send emails on submit
-	requires Amazon SES for AWS (https://aws.amazon.com/ses/)
+## Description
 
+The main goal is to make drop-in tutoring services offered by various departments more efficient. With the shared knowledgebase, tutors can pass back and forth information they find useful in providing assistance to those in need. The feedback and reporting features allow for department heads to quickly get an understanding of how frequently the drop-in tutoring service is used. Finally, students will only need to look at one location to view all of the drop-in tutoring sessions offered campus-wide. By streamlining how individual tutoring sessions are tracked, department heads will be able to get a better understanding of how effective the drop-in tutors are in providing assistance to those in need. 
+
+
+### Relevant Folders
+1. api / APIClient - contains all files for the API implementation
+2. public - contains all files that comprise the web application
+3. documentation - all documentation that was to be submitted throughout the semester
+4. page views - designs for each page of the site
+5. database schema - database schema stored in spreadsheet format
+	
+### Team Members
+   Front-end: Bryan Nunez, Aanchal Chaturvedi, Eric Carpizo, Huy Ly, Jeremiah Caban
+   Back-end: Chris Mariani, Tim Baker
---
+++ TO_BE_REMOVED/register.php
@@ -0,0 +1,94 @@
+<?php
+	$title = "Register";
+	include("header.php");
+?>
+   <!--Yellow bar -->
+    <div class="w3-container w3-yellow">
+       <p></p>  
+    </div>
+        <!--add nav bar -->
+    <div class="w3-container">
+        
+    </div>
+	<div class="w3-bar w3-border w3-light-grey">
+	<?php
+    include("navbar.php"); 
+    ?>
+	</div>
+	
+	<div class="w3-container w3-brown" >
+    <h2>Registration Form</h2>
+    </div>
+	<img src ="bin/images/background1.jpg" alt = "Background" style="width:100%">
+	<!--- Form begins here ----->
+	<div class = "w3-container w3-light-grey w3-display-middle" style = "width:30%; height:60%">
+	<form id="register" action="register.php" method="post">
+	<label>First Name</label> 
+    <input class="w3-input w3-border w3-round" type="text" name="firstname">
+  
+	<label>Last Name</label>
+	<input class="w3-input w3-border w3-round" type="text" name="lastname">
+    
+	<label>User Name</label>
+	<input class="w3-input w3-border w3-round" type="text" name="username">
+    
+	<label>Enter Password</label>
+    <input class="w3-input w3-border w3-round" type="password" name="password">
+    
+	<label>Admin</label>
+	<input class="w3-radio" type = "radio" name="isAdmin" value = "yes">
+	<label>Yes</label>
+	<input class = "w3-radio" type = "radio" name = "isAdmin" value = "no">
+	<label>No</label><br>
+		
+	<label>Allow notification </label>
+	<input class="w3-radio" type="radio" name="notify" value = "yes">
+	<label>Yes</label>
+	<input class = "w3-radio" type = "radio" name = "notify" value = "no">
+	<label>No</label>
+    <p>
+    <input type="submit" name="submit"  class="w3-block w3-brown w3-display-bottom-middle w3-large">
+    </p>
+    <?php
+	require_once("../APIClient.php");
+	if(isset($_POST['submit']))
+	{
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		$firstname = $_POST["firstname"];
+		$lastname = $_POST["lastname"];
+		$isAdmin = false;
+		if($_POST["isAdmin"] == "yes")
+		{
+			$isAdmin = true;
+		}
+		$notify = false;
+		if($_POST["notify"] == "yes")
+		{
+			$notify = true; 
+		}
+		
+		$user = new User(null, $username, $firstname, $lastname, $isAdmin, $notify);
+		APIClient::addUser($user, $password);
+	}
+	?>	
+   
+      
+	
+    </form><br>
+		
+		
+	
+	</div>
+	
+	<?php
+	include("footer.php");
+	?>
+</body>
+</html>
+	
+		 
+		
+	
+  
+	
